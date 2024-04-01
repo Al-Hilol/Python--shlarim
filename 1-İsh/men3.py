a9c9c831328d7492f471500a52071ad67e1da35d
Al-Hilol <abduqodirhalimjonzod@gmail.com>
2024-04-01 22:54:10 +0300

    Navbatdagi fayllar
    Keyingi fayllar

---
+++ 1-İsh/men3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 22:47:51 2024
+
+@author: Al Hilol
+"""
+
+#bir=int(input('1-sonni kiriting! :'))
+#ikki=int(input('2-sonni kiriting!:'))
+#if bir==ikki:
+#    print('Sonlar teng!')
+#else:
+#    print('Sonlar teng emas,qayta uring !')
+
+#son=input('İxtiyoriy son kiriting! :')
+#if son>='0':
+#    print('Kiritilgan son Musbat !')
+#else:
+#    print('Kiritilgan son Manfiy !')
---
+++ men3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 22:47:51 2024
+
+@author: Al Hilol
+"""
+
